dff0352fd568cf6003bfecdf6968d1571a6feac1
AropJoe <josephbusa@outlook.com>
2023-05-17 21:48:16 +0100

    update core/admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Project)
+admin.site.register(models.Data)
+admin.site.register(models.File)
+admin.site.register(models.Analysis)
