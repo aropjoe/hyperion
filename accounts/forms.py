04823cc54d3e86718db39b65fcc673b314aab2d0
AropJoe <josephbusa@outlook.com>
2023-05-17 21:50:33 +0100

    add accounts/forms.py

---
+++ accounts/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User, Business
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+
+class BusinessRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Business
+        fields = ("name", "email", "password")
+
+
+class BusinessUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ("name", "email")
