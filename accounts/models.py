a6e072534e37bb109542cb8e6864957281e7c5c9
AropJoe <josephbusa@outlook.com>
2023-05-17 21:47:58 +0100

    update accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
