a6e9a0b9069e175cb81f4819797ca9eba2595018
AropJoe <josephbusa@outlook.com>
2023-05-19 23:58:07 +0100

    update accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -40,34 +40,36 @@ def update_business(request):
 
 
 def register_user(request):
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username)
+        user = User.objects.create_user(
+            first_name=first_name, last_name=last_name, email=email, username=username
+        )
         user.set_password(password)
         user.save()
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('core:dashboard')
+            return redirect("core:dashboard")
         else:
-            messages.error(request, 'An error occured')
+            messages.error(request, "An error occured")
 
-    return render(request, 'accounts/register.html') 
+    return render(request, "accounts/register.html")
 
 
 @login_required
 def update_user(request):
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        username = request.POST.get('username')
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        username = request.POST.get("username")
 
         user = request.user
 
@@ -78,30 +80,30 @@ def update_user(request):
 
         user.save()
 
-        return redirect('core:dashboard')
+        return redirect("core:dashboard")
 
     else:
-        return render(request, 'accounts/update_user.html') 
+        return render(request, "accounts/update_user.html")
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('core:dashboard')
+            return redirect("core:dashboard")
         else:
-            messages.error(request, 'Invalid username or password.')
+            messages.error(request, "Invalid username or password.")
 
-    return render(request, 'accounts/login.html') 
+    return render(request, "accounts/login.html")
 
 
 def logout_view(request):
     logout(request)
-    return redirect('accounts:login')  
+    return redirect("accounts:login")
 
 
 @login_required
