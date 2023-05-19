b04e7a581d55bebcdc1ed25aad20438564cfbc72
AropJoe <josephbusa@outlook.com>
2023-05-19 14:39:44 +0100

    write simple dashboard view

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,3 +1,11 @@
 from .analysis import *
 from .data import *
 from .project import *
+
+
+def dashboard(request):
+    context = {
+        "user": request.user
+    }
+    template = "core/dashboard.html"
+    return render(request, template, context)
