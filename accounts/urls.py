7170c420037d9ee35e045749f3215d7d4279d79c
AropJoe <josephbusa@outlook.com>
2023-05-19 14:38:44 +0100

    write accounts urls

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register/business/', views.register_business, name='register_business'),
+    path('update/business/', views.update_business, name='update_business'),
+    path('register/user/', views.register_user, name='register_user'),
+    path('update/user/', views.update_user, name='update_user'),
+    path('profile/', views.user_profile, name='user_profile'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
