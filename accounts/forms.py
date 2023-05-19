e6c362c232bc1de6c9fe4f10bb4bbce1756e7583
AropJoe <josephbusa@outlook.com>
2023-05-19 14:38:24 +0100

    update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import User, Business
-
+from .models import Business
+from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
