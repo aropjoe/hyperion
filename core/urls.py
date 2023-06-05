431e74ddf656a9910e0f32f840b261094374c21f
Joe Arop <josephbusa@outlook.com>
2023-06-05 11:47:10 +0100

    add route for new purchase orders analysis

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from .views import (
     conversion_analytics,
     performance_metrics,
     subscription_analytics,
+    po_analysis,
 )
 
 app_name = "core"
@@ -33,6 +34,7 @@ urlpatterns = [
     path("data/create/", data_create, name="data_create"),
     path("data/<int:data_id>/", data_detail, name="data_detail"),
     path("data/<int:data_id>/analysis/", data_analysis, name="data_analysis"),
+    path("data/<int:data_id>/po-analysis/", po_analysis, name="po_analysis"),
     path("data/<int:data_id>/text-analysis/", text_analysis, name="text_analysis"),
     path("data/<int:data_id>/subscription-analytics/", subscription_analytics, name="subscription_analytics"),
     path(
